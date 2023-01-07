6e7f4afc9397571e07f38a3e536d95f70e650089
Seb <jonesseb10@gmail.com>
2023-01-07 16:29:43 +0000

    added do while loop for generatePassword func

--- js/script.js
+++ js/script.js
@@ -19,21 +19,20 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-  // Prompt to get number of characters for password
-  var numChar = prompt("How many characters would you like your password? Min of 10, max of 64 (including 64)");
-  // If invalid num - let user know and return
-  if (numChar <10 | numChar > 64){
-    alert("Invalid number - please try again");
-    return;
-  }
-
   // Object to hold user input
   characteristics={
+    numChar: prompt("How many characters would you like your password? Min of 10, max of 64 (including 64)"),
     lowerCase: confirm("Would you like lowercase letters? (OK = yes, cancel = no)"),
     upperCase: confirm("Would you like uppercase (OK = yes, cancel = no)"),
     includeNum: confirm("Would you like numbers? (OK = yes, cancel = no)"),
     specialChar: confirm("Would you like special characters? (OK = yes, cancel = no)")
   };
+
+  // If invalid num - let user know and return
+  if (characteristics.numChar <10 | characteristics.numChar > 64){
+    alert("Invalid number of characters - please try again");
+    return;
+  }
  
   // Check if password contains at least one option chosen by user - if none - alert user 
   if (!characteristics.lowerCase && !characteristics.upperCase && !characteristics.includeNum && !characteristics.specialChar){
@@ -52,21 +51,49 @@ function getRandom(arr) {
 }
 
 // So far - got user inputs & can generate a random element from arrays of characters
-// Now; 
+
+// Now;
 // Need to generate password based on user input
+  // How?
+// Take a random element each time from the selected inputs
+// Each time based off true inputs 
+
 
 
 
 // Function to generate password with user input
 function generatePassword() {
   
+  // Run func to get password options
+
   getPasswordOptions();
 
-  var trueChars = [];
-  for (let i in characteristics) {
-      trueChars.push(characteristics[i]);
-  }
+  var trueChars = (Object.entries(characteristics));
   
+  var madePassword = [];
+
+// Do .. while loop to run random numbers whilst the madePassword
+//  array is less than number of characters seleceted by user
+
+  do {
+    if (trueChars[1][1] === true){
+      madePassword.push(getRandom(lowerCasedCharacters));
+    }
+    if (trueChars[2][1] === true){
+      madePassword.push(getRandom(upperCasedCharacters));
+    }
+    if (trueChars[3][1] === true){
+      madePassword.push(getRandom(numericCharacters));
+    }
+    if (trueChars[4][1] === true){
+      madePassword.push(getRandom(specialCharacters));
+    }
+  } while (madePassword.length < characteristics.numChar);
+
+  
+  console.log(madePassword.toString());
+
+
 
 
 }
